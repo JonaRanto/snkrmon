23edeb82c40da39c0afff427bb1fed32d2f4637b
Jonathan Navarro <olivergt123@gmail.com>
2022-05-16 17:13:27 -0400

    update v2.2.1

--- pyinstaller.py
+++ pyinstaller.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 with open('README.md') as f:
     lines = f.read()
@@ -8,8 +7,8 @@ with open('README.md') as f:
 
 current_path = os.getcwd()
 
-os.system('pyinstaller main.py -n snkrmon-' + version + ' --specpath=specs --icon=' + current_path + '\icon.ico --exclude-module=alarm_control --exclude-module=auto_purchase --exclude-module=pyinstaller --exclude-module=wd --add-data=..\\config.ini;. --add-data=..\\icon.ico;. --add-data=..\\alarm.wav;. --add-data=..\\auto_purchase.py;. --add-data=..\\alarm_control.py;. --add-data=..\\log_control.py;. --add-data=..\\wd.py;. --noconfirm')
+os.system('pyinstaller main.py -n snkrmon-' + version + ' --specpath=specs --icon=' + current_path + '\icon.ico --exclude-module=alarm_control --exclude-module=auto_purchase --exclude-module=pyinstaller --exclude-module=wd --add-data=..\\config.ini;. --add-data=..\\icon.ico;. --noconfirm')
 
-os.system('pyinstaller auto_purchase.py -n run --specpath=specs --icon=' + current_path + '\icon.ico --exclude-module=gui --exclude-module=main --exclude-module=pyinstaller --add-data=..\\config.ini;. --add-data=..\\icon.ico;. --add-data=..\\alarm.wav;. --add-data=..\\auto_purchase.py;. --add-data=..\\alarm_control.py;. --add-data=..\\log_control.py;. --add-data=..\\wd.py;. --noconfirm')
+os.system('pyinstaller auto_purchase.py -n run --specpath=specs --exclude-module=gui --exclude-module=main --exclude-module=pyinstaller --add-data=..\\config.ini;. --add-data=..\\alarm.wav;. --noconfirm')
 
 os.system('robocopy dist\\run dist\\snkrmon-' + version + ' /E')
